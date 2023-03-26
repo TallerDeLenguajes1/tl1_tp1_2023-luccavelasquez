86cf3a77800c67ab1d8cf3563dcd3629a18e3101
lucas <lucasvelasquez2001@gmail.com>
2023-03-26 12:38:08 -0300

    agregando cambios

--- tp1_1.c
+++ tp1_1.c
@@ -2,8 +2,21 @@
 
 int main()
 {
+    int num=10;
+    int *punt= &num;
 
     printf("Hola mundo");
+    
+    //el contenido del puntero
+    printf("\n%d",*punt);
+    //la direccion de memoria almacenada por el punt
+    printf("\n%d",punt);
+    //la direccion de memoria de la variable
+    printf("\n%d",&num);
+    //la direccion de memoria del puntero
+    printf("\n%d",&punt);
+    //el tamaño de memoria 
+    printf("\n%d", sizeof(punt));
 
     return 0;
 }
